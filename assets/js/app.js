589a5c2d86e69d4dd3211d5fc48b96d43152c596
MartinaGangemi <martinagangemi94@gmail.com>
2022-04-22 14:34:31 +0200

    versione senza css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,46 @@
 const app = new Vue({
     el:"#app",
     data:{
-
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+        newTask:"",
+       
         tasks:[
             {
                 text:"fare gli esercizi",
-                done:"false",
+                done:false,
             },
             {
                 text:"cucinare",
-                done:"true",
+                done:true,
             },
             {
                 text:"pulire casa",
-                done:"false",
+                done:false,
             }
             
         ]
     },
     methods:{
+//         MILESTONE 2
+//  Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
         removeTask(index){
             this.tasks.splice(index,1);
-}
-    }
+        },
+
+
+//         MILESTONE 3
+//  Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+        
+        addTask(){
+            if(this.newTask.length > 0) {
+                task = {text: this.newTask, done:false}
+                this.tasks.unshift(task),
+                this.newTask=""
+
+            }
+            
+        }
+
+        }
 })
