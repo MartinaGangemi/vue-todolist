5f29d75554d67b5770057e78f728ab8c25a01002
MartinaGangemi <martinagangemi94@gmail.com>
2022-04-22 12:32:46 +0200

    milestone1

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+const app = new Vue({
+    el:"#app",
+    data:{
+
+        tasks:[
+            {
+                text:"fare gli esercizi",
+                done:"false",
+            },
+            {
+                text:"cucinare",
+                done:"true",
+            },
+            {
+                text:"pulire casa",
+                done:"false",
+            }
+            
+        ]
+    },
+    method:{
+
+    }
+})
